5be1c1d267e71e215e8db9eead2c7d052bce8fc1
Dylan Chen <shuochen0113@gmail.com>
2025-01-09 14:49:21 +0800

    Removed old odgi folder

--- odgi/default.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.callPackage ./odgi.nix {
-  inherit (pkgs) stdenv fetchFromGitHub cmake jemalloc pkg-config python3 gcc zlib htslib gsl;
-}
--- odgi/src/position.cpp
+++
@@ -1,9 +0,0 @@
-#include "position.hpp"
-
-namespace odgi {
-
-std::ostream& operator<<(std::ostream& out, const pos_t& pos) {
-    return out << id(pos) << (is_rev(pos) ? "-" : "+") << offset(pos);
-}
-
-}
